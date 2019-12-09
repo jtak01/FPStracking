cde2b850eb80d376d5e08e6919eebc78fb37d608
Jun Tak <54953784+jtak01@users.noreply.github.com>
2019-12-08 21:25:18 -0500

    Add files via upload

---
+++ trackSound.js
@@ -0,0 +1,34 @@
+let mic;
+    var amplitude;
+    var context;
+    var onAndOff = false;
+    // window.onload = function() {
+    function setupMic(){
+        console.log("running");
+        //context = new AudioContext();
+        mic = new p5.AudioIn();
+        mic.start();
+        console.log(mic.getSources());
+        console.log(mic.input);
+    }
+
+    function getNoiseLevel(){
+        document.getElementById("bang").innerHTML = "";
+        micLevel = mic.getLevel();
+        console.log(micLevel * 100);
+        document.getElementById("hi").innerHTML = micLevel * 100;
+        if (micLevel * 100 >= 16){
+            Shoot();
+            console.log("real loud");
+            document.getElementById("bang").innerHTML = "Shot!";
+        }
+    }
+
+    function record(){
+        onAndOff = !onAndOff;
+        console.log("record()");
+        // context = new AudioContext();
+        getAudioContext().resume();
+        //canvas.getAudioContext().resume();
+        setInterval(getNoiseLevel, 200);
+    }
